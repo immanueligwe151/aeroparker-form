25710e1ec0d156a5ae38bf9a2283b5aeccdd487b
Immanuel <64789685+immanueligwe151@users.noreply.github.com>
2025-03-04 10:31:08 +0000

    generated maven files for spring boot application

---
+++ parkerform/src/main/java/com/aeroparker/parkerform/ParkerformApplication.java
@@ -0,0 +1,13 @@
+package com.aeroparker.parkerform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkerformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkerformApplication.class, args);
+	}
+
+}
---
+++ parkerform/src/test/java/com/aeroparker/parkerform/ParkerformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aeroparker.parkerform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkerformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
